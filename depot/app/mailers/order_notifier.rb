6ff4d57809b93e83e350793f3b5322dd24aef207
tomgdow <thomasgdowling@gmail.com>
2015-03-03 16:07:58 +0000

    first commit of depot code

---
+++ depot/app/mailers/order_notifier.rb
@@ -0,0 +1,27 @@
+class OrderNotifier < ApplicationMailer
+
+  default from: 'Jack Ruby <lho@youshotme.com'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+    @greeting = "Hi"
+
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
